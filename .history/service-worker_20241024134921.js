76054e7c4210f7989445b6f40cd2062a2d3256bb
Sami <eugproductions@gmail.com>
2024-10-24 13:50:56 +0200

    modofications cursor

---
+++ .history/service-worker_20241024134921.js
@@ -0,0 +1,65 @@
+const CACHE_NAME = 'xidada-cache-v1';
+const urlsToCache = [
+  '/',
+  '/index.html',
+  '/manifest.json',
+  '/icons/icon-72x72.png',
+  '/icons/icon-96x96.png',
+  '/icons/icon-128x128.png',
+  '/icons/icon-144x144.png',
+  '/icons/icon-152x152.png',
+  '/icons/icon-192x192.png',
+  '/icons/icon-384x384.png',
+  '/icons/icon-512x512.png',
+  'https://cdn.jsdelivr.net/npm/@ionic/core/css/ionic.bundle.css',
+  'https://cdn.jsdelivr.net/npm/ionicons@5.5.2/dist/css/ionicons.min.css',
+  'https://cdn.jsdelivr.net/npm/@ionic/core/dist/ionic/ionic.esm.js',
+  'https://cdn.jsdelivr.net/npm/@ionic/core/dist/ionic/ionic.js',
+  'https://cdn.jsdelivr.net/npm/@supabase/supabase-js@2'
+];
+
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then(cache => cache.addAll(urlsToCache))
+  );
+});
+
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request)
+      .then(response => {
+        if (response) {
+          return response;
+        }
+        return fetch(event.request).then(
+          response => {
+            if(!response || response.status !== 200 || response.type !== 'basic') {
+              return response;
+            }
+            const responseToCache = response.clone();
+            caches.open(CACHE_NAME)
+              .then(cache => {
+                cache.put(event.request, responseToCache);
+              });
+            return response;
+          }
+        );
+      })
+  );
+});
+
+self.addEventListener('activate', event => {
+  const cacheWhitelist = [CACHE_NAME];
+  event.waitUntil(
+    caches.keys().then(cacheNames => {
+      return Promise.all(
+        cacheNames.map(cacheName => {
+          if (cacheWhitelist.indexOf(cacheName) === -1) {
+            return caches.delete(cacheName);
+          }
+        })
+      );
+    })
+  );
+});
